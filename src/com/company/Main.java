af8911e65820ccac6488ea6aaddb76de2cf19aed
wolfanderson <526417787@qq.com>
2019-06-26 21:48:35 +0800

    hello 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.print("hello pig");
+	System.out.println("hello pig");
+        System.out.println("hello pig2");
     }
 }
